89badbe47f95702f6593f41ca2f8e2c94290a290
Cherakala Gireesh <135722119+cherakalagireesh@users.noreply.github.com>
2023-12-05 15:50:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Print Anagrams Together/print-anagrams-together.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	    int t=Integer.parseInt(br.readLine().trim());
+	    while(t > 0)
+	    {
+	        int n= Integer.parseInt(br.readLine().trim());
+	        String x = br.readLine().trim();
+	        String string_list[] = x.split(" ",n);
+	        
+	        Solution ob = new  Solution();
+	        
+	        List <List<String>> ans = ob.Anagrams(string_list);
+	        
+	        Collections.sort(ans, new Comparator<List<String>>(){
+            public int compare(List<String> l1, List<String> l2) {
+                    String s1 =  l1.get(0);
+                    String s2 = l2.get(0);
+                    
+                    return s1.compareTo(s2);
+                }
+            });
+	        
+	        for(int i=0;i<ans.size();i++)
+	        {
+	            for(int j=0;j<ans.get(i).size();j++)
+	            {
+	                System.out.print(ans.get(i).get(j) + " ");
+	            }
+	            System.out.println();
+	        }
+	       
+	       
+            t--;
+	    }
+	}
+    
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public List<List<String>> Anagrams(String[] string_list) {
+          
+        List<List<String>> result = new ArrayList<>();
+        Map<String,List<String>> map = new HashMap<>();
+        
+        for(String str:string_list)
+        {
+            char ch[] = str.toCharArray();
+            Arrays.sort(ch);
+            String s = new String(ch);
+            
+            List<String> valueList = map.getOrDefault(s,new ArrayList<>());
+            valueList.add(str);
+            map.put(s,valueList);
+        }
+        
+        for(List<String> elements:map.values())
+        {
+            result.add(elements);
+        }
+        
+        return result;
+    }
+}
