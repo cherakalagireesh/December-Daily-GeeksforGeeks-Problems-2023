67313a83e64e81778ac5cf62989e69ce3a58a155
Cherakala Gireesh <135722119+cherakalagireesh@users.noreply.github.com>
2023-12-09 16:14:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Next Permutation/next-permutation.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String a[] = in.readLine().trim().split("\\s+");
+            int arr[] = new int[N];
+            for(int i = 0;i < N;i++)
+                arr[i] = Integer.parseInt(a[i]);
+            
+            Solution ob = new Solution();
+            List<Integer> ans = new ArrayList<Integer>();
+            ans = ob.nextPermutation(N, arr);
+            StringBuilder out = new StringBuilder();
+            for(int i = 0;i < N;i++)
+                out.append(ans.get(i) + " ");
+            System.out.println(out);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static List<Integer> nextPermutation(int N, int arr[]){
+        
+        int index = -1;
+        List<Integer> result = new ArrayList<>();
+        
+        for(int i=N-2;i>=0;i--)
+        {
+            if(arr[i] < arr[i+1])
+            {
+                index = i;
+                break;
+            }
+        }
+        
+        if(index == -1)
+        {
+             return reverseArray(result,arr);
+        }
+        
+        for(int i=N-1;i>index;i--)
+        {
+            if(arr[i] > arr[index])
+            {
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+                break;
+            }
+        }
+        
+        Arrays.sort(arr,index+1,N);
+        
+        return insert(result,arr,N) ;
+    }
+    
+    public static List<Integer> insert(List<Integer> result,int arr[],int length)
+    {
+        
+        for(int num:arr)
+        {
+            result.add(num);
+        }
+        
+        return result;
+    }
+    
+    public static List<Integer> reverseArray(List<Integer> result,int arr[])
+    {
+        int length = arr.length;
+        for(int i=length-1;i>=0;i--)
+        {
+            result.add(arr[i]);
+        }
+        
+        return result;
+    }
+}
